9882d7da670f8f45f3431e3c7da80a00d6af4b5e
Gudmundur Haraldsson <gdh1@hi.is>
2019-03-05 17:41:27 +0000

    Adding test file

---
+++ my-test-file-1.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "mystring1" . time() . PHP_EOL;
+
+echo esc_html( time() . PHP_EOL );
+
+echo "mystring2" . time() . PHP_EOL;
+
+echo esc_html( time() . PHP_EOL );
+
+echo "mystring3" . time() . PHP_EOL;
+
+echo esc_html( time() . PHP_EOL );
+
