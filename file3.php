027de6d804e1d40dbe1b13a3ede7cfa758787b85
Gudmundur Haraldsson <gdh1@hi.is>
2020-03-31 12:26:34 +0000

    test

---
+++ file3.php
@@ -0,0 +1,17 @@
+<?php
+
+// Some output
+echo esc_html( "foo1" . PHP_EOL );
+
+// Testing
+echo esc_html( "foo2" . PHP_EOL ); // Some stuff
+
+// Testing again
+echo esc_html( "foo2" . PHP_EOL ); // Some other stuff
+
+
+/*
+ * Comment.
+ */
+
+// Other comment
