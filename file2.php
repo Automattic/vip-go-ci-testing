fc2cf630fbf1cb1950ae82118be0834c7876f431
Gudmundur Haraldsson <gdh1@hi.is>
2020-03-31 12:28:14 +0000

    Changing files

--- file2.php
+++ file2.php
@@ -1,12 +1,7 @@
 <?php
 
-// Some output
 echo esc_html( "foo1" . PHP_EOL );
-
-// Testing
 echo esc_html( "foo2" . PHP_EOL ); // Some stuff
-
-// Testing again
 echo esc_html( "foo2" . PHP_EOL ); // Some other stuff
 
 
