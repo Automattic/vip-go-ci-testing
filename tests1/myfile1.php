732e62fc7bd28a1a63a724cf45c893f2b71830c2
Ariana Kataoka <aris.kataoka@gmail.com>
2021-07-06 16:24:59 +1000

    Test - Add file to test skip-large-files functionality

---
+++ tests1/myfile1.php
@@ -0,0 +1,8 @@
+<?php
+
+echo $_POST[ 'phpcs should catch this problem' ];
+echo 'linter will catch this fatal;
+echo 'linter will catch this fatal;
+echo 'linter will catch this fatal;
+echo 'linter will catch this fatal;
+echo 'linter will catch this fatal;
